de72b4d0bc4c2c9c01d626f2247ba825c1ca3a4b
khis@stibodx.com <khis@stibodx.com>
2022-04-24 17:33:41 +0600

    add repository class to get github repo

---
+++ app/src/main/java/com/techascent/cleanarchitecture2/data/repo/GitRepoRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package com.techascent.cleanarchitecture2.data.repo
+
+class GitRepoRepositoryImpl {
+}
---
+++ app/src/main/java/com/techascent/cleanarchitecture2/domain/repo/GitRepoRepository.kt
@@ -0,0 +1,4 @@
+package com.techascent.cleanarchitecture2.domain.repo
+
+interface GitRepoRepository {
+}
